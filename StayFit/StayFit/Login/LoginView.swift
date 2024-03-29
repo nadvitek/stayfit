b8989ddb6232f93aa8f2753757f1c4fb4fdff3a7
Vít Nademlejnský <nadvitek@gmail.com>
2024-03-29 15:06:13 +0100

    ✨LoginView kicked off

--- StayFit/StayFit/Login/LoginView.swift
+++ StayFit/StayFit/Login/LoginView.swift
@@ -1,15 +1,16 @@
-//
-//  LoginView.swift
-//  StayFit
-//
-//  Created by Vít Nademlejnský on 29.03.2024.
-//
-
 import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Text("Login")
+                .font(.semiLarge)
+                .foregroundStyle(.black)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("")
+            }
+        }
     }
 }
 
--- StayFit/StayFit/Login/LoginViewModel.swift
+++ StayFit/StayFit/Login/LoginViewModel.swift
@@ -1,8 +1,29 @@
-//
-//  LoginViewModel.swift
-//  StayFit
-//
-//  Created by Vít Nademlejnský on 29.03.2024.
-//
-
 import Foundation
+import Observation
+
+protocol LoginViewModeling {
+    var email: String { get set }
+    var password: String { get set }
+    
+    func login()
+}
+
+@Observable
+class LoginViewModel: LoginViewModeling {
+    
+    // MARK: - Internal properties
+    
+    var email: String = ""
+    var password: String = ""
+    
+    // MARK: - Initializers
+    
+    init() {
+    }
+    
+    // MARK: - Internal interface
+    
+    func login() {
+        // TODO: - Implement later
+    }
+}
